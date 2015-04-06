0e033b38a57e949782df108f5c78d49cdd73e490
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-04-05 21:00:56 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+release=1
+
+curdir="$(pwd)"
+filesdir="$(cd "$(dirname "${0}")" && pwd)"
+
+pushd "${filesdir}"
+
+version="$(cat system/CSCVersion.txt)"
+sales_code="$(cat system/csc/sales_code.dat)"
+
+zip -r "${curdir}/${sales_code}_${version}_r${release}.zip" \
+    META-INF system
+
+popd
